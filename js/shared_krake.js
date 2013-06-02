cf323ca64943c83e7a93ec6039c83ff9a2c238a4
Joseph Yang <sirjosephyang@gmail.com>
2013-06-02 18:03:19 +0800

    Session state machine implemented

--- js/shared_krake.js
+++ js/shared_krake.js
@@ -1,13 +1,31 @@
- /*
-  * 'idle' => no column object attached
-  * 'ready_for_selection' => a column object is in stage area for edit
-  * 'ready_for_selection' => a column is added and ready for edit
-  * 'first_column_selected' => xpath1 is selected
-  * 'second_column_selected' => xpath2 is selected
-  * 'ready_for_pagination_selection' => before pagination is defined
-  */
-var SessionManager = function()
-{
+/*
+ * Copyright 2013 Krake Pte Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author:
+ * Joseph Yang <sirjosephyang@krake.io>
+ */
+
+/*
+ * 'idle' => no column object attached
+ * 'ready_for_selection' => a column object is in stage area for edit
+ * 'ready_for_selection' => a column is added and ready for edit
+ * 'first_column_selected' => xpath1 is selected
+ * 'second_column_selected' => xpath2 is selected
+ * 'ready_for_pagination_selection' => before pagination is defined
+ */
+var SessionManager = function(){
   this.currentState = 'idle';
 
   this.parentColumnId = null;
